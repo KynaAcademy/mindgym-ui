bf38b1c465a7e5509ede516b29dbd6436ef7165e
Wouter de Vos <309331+wearethefoos@users.noreply.github.com>
2022-06-17 11:59:06 +0200

    feat(Highlight): Add primary highlight color

--- src/components/Typography/Highlight.stories.tsx
+++ src/components/Typography/Highlight.stories.tsx
@@ -26,3 +26,9 @@ const Template = (args: TitleProps) => (
 export const Normal = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Normal.args = {};
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  primary: true,
+};
