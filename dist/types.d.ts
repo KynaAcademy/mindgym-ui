9af773c9462fd8723d6c24850f782d5fe32020d7
Wouter de Vos <309331+wearethefoos@users.noreply.github.com>
2022-06-27 19:33:58 +0200

    dev: Add dist to source control

---
+++ dist/types.d.ts
@@ -0,0 +1,98 @@
+import { ReactNode, FunctionComponent, AnchorHTMLAttributes, ButtonHTMLAttributes } from "react";
+type ThemeProviderProps = {
+    colorScheme?: any;
+    children: ReactNode;
+};
+type UIMode = "light" | "dark" | "os";
+export type ThemeContext = {
+    mode: UIMode;
+    colorScheme: any;
+    resolvedMode: "light" | "dark";
+    overrideMode: (mode: UIMode) => void;
+    resetMode: () => void;
+};
+export const Theme: import("react").Context<ThemeContext>;
+export const ThemeProvider: ({ children, colorScheme }: ThemeProviderProps) => import("@emotion/react/jsx-runtime").JSX.Element;
+type SVGProps = {
+    mono?: boolean;
+    className?: string;
+    darkBackground?: boolean;
+};
+export type LogoProps = {
+    variant?: "horizontal" | "pre-horizontal" | "dots" | "default";
+} & SVGProps;
+export const Logo: FunctionComponent<LogoProps>;
+export const DefaultLogo: FunctionComponent<SVGProps>;
+export const HLogo: ({ mono, darkBackground, ...props }: SVGProps) => import("@emotion/react/jsx-runtime").JSX.Element;
+export const HLogoPre: ({ mono, darkBackground, ...props }: SVGProps) => import("@emotion/react/jsx-runtime").JSX.Element;
+export const LogoDots: ({ mono, ...props }: SVGProps) => import("@emotion/react/jsx-runtime").JSX.Element;
+type BullitType = "numbers" | "checks" | ReactNode;
+export type GuideProps = {
+    title: ReactNode;
+    items: ReactNode[];
+    bullit?: BullitType;
+};
+export const Guide: FunctionComponent<GuideProps>;
+export type ButtonSize = "large" | "small";
+type SharedButtonProps = {
+    primary?: boolean;
+    secondary?: boolean;
+    label?: string;
+    children?: ReactNode;
+    size?: ButtonSize;
+    darkMode?: boolean;
+};
+type ButtonPropsForButtonElement = {
+    href?: string;
+} & SharedButtonProps & Partial<ButtonHTMLAttributes<HTMLButtonElement>>;
+type ButtonPropsForLink = {
+    href: string;
+} & SharedButtonProps & Partial<AnchorHTMLAttributes<HTMLAnchorElement>>;
+export type ButtonProps = ButtonPropsForButtonElement | ButtonPropsForLink;
+export const AsButton: any;
+export const AsAnchor: any;
+export const Button: FunctionComponent<ButtonProps>;
+export type HeroProps = {
+    title: string;
+    leader?: string;
+    follower?: string;
+    image?: string;
+    imageAlt?: string;
+    primaryCTA: {
+        label: string;
+        href: string;
+    };
+    secondaryCTA?: {
+        label: string;
+        href: string;
+    };
+    header?: ReactNode;
+};
+export const Hero: FunctionComponent<HeroProps>;
+export function classNames(...classes: string[]): string;
+export type StakeProps = {
+    title: ReactNode;
+    subtitle?: ReactNode;
+    image?: string;
+    imageAlt?: string;
+};
+export const Stake: FunctionComponent<StakeProps>;
+export type HighlightProps = {
+    primary?: boolean;
+    children: ReactNode;
+};
+export const Highlight: FunctionComponent<HighlightProps>;
+type NavbarLink = {
+    href: string;
+    label: string;
+    active?: boolean;
+};
+export type NavbarProps = {
+    links: NavbarLink[];
+    darkBackground?: boolean;
+    transparent?: boolean;
+};
+export const Navbar: FunctionComponent<NavbarProps>;
+export const GlobalStyles: () => import("@emotion/react/jsx-runtime").JSX.Element;
+
+//# sourceMappingURL=types.d.ts.map
