ad0e9a29e1c23dd31f9a15e22a9a2ebb3fa38999
nemoprime86 <81518686+StevenMater@users.noreply.github.com>
2023-01-06 02:56:14 +0100

    fix(Twin.Macro): refactored to TSC

--- src/components/Marketing/Guide.tsx
+++ src/components/Marketing/Guide.tsx
@@ -1,6 +1,6 @@
 import { FunctionComponent, ReactNode } from "react";
-import { useMode } from "../../hooks/useMode";
-import tw, { styled } from "twin.macro";
+
+import tw from "tailwind-styled-components";
 
 type BullitType = "numbers" | "checks" | ReactNode;
 
@@ -19,26 +19,18 @@ const Bullit: FunctionComponent<BullitProps> = ({ type, num }) => {
   switch (type) {
     case "numbers":
       return (
-        <span tw="flex rounded-full bg-pink-600 text-white p-4 w-6 h-6 mr-2 items-center">
-          <span tw="block -ml-1">{num}</span>
-        </span>
+        <BullitWrapper>
+          <NumWrapper>{num}</NumWrapper>
+        </BullitWrapper>
       );
 
     case "checks":
-      return <span tw="mr-2 text-2xl font-black">✔</span>;
+      return <ChecksWrapper>✔</ChecksWrapper>;
 
     default:
-      return <span tw="mr-2">{type}</span>;
+      return <DefaultWrapper>{type}</DefaultWrapper>;
   }
 };
-
-const Section = styled.section(() => {
-  const mode = useMode();
-  return [
-    tw`p-0 m-0 text-slate-900 dark:text-slate-200`,
-    mode(tw`text-slate-900`, tw`text-slate-200`),
-  ];
-});
 
 export const Guide: FunctionComponent<GuideProps> = ({
   title,
@@ -46,26 +38,89 @@ export const Guide: FunctionComponent<GuideProps> = ({
   bullit,
 }) => {
   return (
-    <Section>
-      <div tw="container">
-        <h1 tw="text-3xl font-extrabold font-serif capitalize sm:text-4xl lg:text-5xl lg:text-center">
-          {title}
-        </h1>
-        <article tw="w-full lg:mx-16 lg:max-w-lg">
-          <ul tw="flex my-8 space-y-2 flex-col">
+    <SectionWrapper>
+      <div className="container">
+        <GuideHeader>{title}</GuideHeader>
+        <GuideArticle>
+          <GuideUl>
             {items.map((item, i) => (
-              <li tw="flex items-center font-black font-sans text-lg" key={i}>
+              <GuideLi key={i}>
                 <Bullit type={bullit} num={i + 1} />
                 {item}
-              </li>
+              </GuideLi>
             ))}
-          </ul>
-        </article>
+          </GuideUl>
+        </GuideArticle>
       </div>
-    </Section>
+    </SectionWrapper>
   );
 };
 
 Guide.defaultProps = {
   bullit: "numbers",
 };
+
+const SectionWrapper = tw.section`
+p-0
+m-0
+text-slate-900
+dark:text-slate-200
+`;
+
+const BullitWrapper = tw.span`
+flex
+rounded-full
+bg-pink-600
+text-white
+p-4
+w-6
+h-6
+mr-2
+items-center
+`;
+
+const NumWrapper = tw.span`
+block
+-ml-1
+`;
+
+const ChecksWrapper = tw.span`
+mr-2
+text-2xl
+font-black
+`;
+
+const DefaultWrapper = tw.span`
+mr-2
+`;
+
+const GuideHeader = tw.h1`
+text-3xl
+font-extrabold
+font-serif
+capitalize
+sm:text-4xl
+lg:text-5xl
+lg:text-center
+`;
+
+const GuideArticle = tw.article`
+w-full
+lg:mx-16
+lg:max-w-lg
+`;
+
+const GuideUl = tw.ul`
+flex
+my-8
+space-y-2
+flex-col
+`;
+
+const GuideLi = tw.li`
+flex
+items-center
+font-black
+font-sans
+text-lg
+`;
