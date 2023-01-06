9affdb4cf64b59674e0c2bc8d88abe86425de6b5
nemoprime86 <81518686+StevenMater@users.noreply.github.com>
2023-01-06 04:25:30 +0100

    fix(Twin.Macro): refactored to TSC

--- src/components/Marketing/Hero.tsx
+++ src/components/Marketing/Hero.tsx
@@ -1,6 +1,8 @@
-import React, { FunctionComponent, ReactNode } from "react";
+import React, { FC, FunctionComponent, ReactNode } from "react";
+
 import { Button } from "../UI/Button";
-import tw, { styled } from "twin.macro";
+import tw from "tailwind-styled-components";
+// import tw, { styled } from "twin.macro";
 import { useMode } from "../../hooks/useMode";
 
 export type HeroProps = {
@@ -22,21 +24,7 @@ export type HeroProps = {
 
 type CTAWrapperProps = { grid?: boolean };
 
-const CTAWrapper = styled.div<CTAWrapperProps>(({ grid }) => [
-  tw`space-y-4`,
-  grid && tw`sm:space-y-0 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5`,
-]);
-
-const Inset = styled.div(() => {
-  const mode = useMode();
-
-  return [
-    tw`absolute inset-x-0 bottom-0 h-1/2 bg-slate-100 dark:bg-slate-800`,
-    mode(tw`bg-slate-100`, tw`bg-slate-800`),
-  ];
-});
-
-export const Hero: FunctionComponent<HeroProps> = ({
+export const Hero: FC<HeroProps> = ({
   header,
   title,
   leader,
@@ -47,51 +35,150 @@ export const Hero: FunctionComponent<HeroProps> = ({
   secondaryCTA,
 }) => {
   return (
-    <div tw="relative">
+    <HeroWrapper>
       <Inset />
       <div tw="max-w-7xl mx-auto">
         <div tw="relative sm:overflow-hidden">
-          <div tw="absolute inset-0">
+          <ImageWrapper>
             {image && (
               <>
-                <img
-                  tw="h-full w-full object-cover"
-                  src={image}
-                  alt={imageAlt || "background image"}
-                />
-                <div tw="absolute inset-0 bg-slate-800 opacity-60 mix-blend-multiply" />
+                <Image src={image} alt={imageAlt || "background image"} />
+                <ImageDiv />
               </>
             )}
-            {!image && (
-              <div tw="absolute inset-0 bg-slate-800 mix-blend-multiply" />
-            )}
-          </div>
-          {header && <div tw="z-50 relative px-4 py-4 -mb-16">{header}</div>}
-          <div tw="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8 text-center">
-            <h1 tw="text-4xl md:max-w-2xl mx-auto font-bold font-serif pb-2 lg:pb-4 tracking-tight">
-              {leader && (
-                <span tw="block font-sans text-white text-lg pb-0">
-                  {leader}
-                </span>
-              )}
-              <span tw="block text-white font-serif mt-4">{title}</span>
-            </h1>
-            {follower && (
-              <p tw="px-2 italic font-light mt-2 max-w-lg mx-auto lg:text-center text-base text-gray-300 sm:max-w-3xl">
-                {follower}
-              </p>
-            )}
-            <div tw="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center">
+            {!image && <NoImageDiv />}
+          </ImageWrapper>
+          {header && <HeaderWrapper>{header}</HeaderWrapper>}
+          <LeaderWrapper>
+            <LeaderH1>
+              {leader && <LeaderSpan>{leader}</LeaderSpan>}
+              <TitleSpan>{title}</TitleSpan>
+            </LeaderH1>
+            {follower && <FollowerParagraph>{follower}</FollowerParagraph>}
+            <ButtonWrapper>
               <CTAWrapper grid={!!secondaryCTA}>
                 <Button primary size="large" {...primaryCTA} />
                 {secondaryCTA && (
                   <Button secondary size="large" {...secondaryCTA} />
                 )}
               </CTAWrapper>
-            </div>
-          </div>
+            </ButtonWrapper>
+          </LeaderWrapper>
         </div>
       </div>
-    </div>
+    </HeroWrapper>
   );
 };
+
+const CTAWrapper = tw.div<CTAWrapperProps>`
+space-y-4
+
+${(p) =>
+  p.grid && "sm:space-y-0 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5"}
+`;
+
+const HeroWrapper = tw.div`
+relative
+`;
+
+const Inset = tw.div`
+absolute
+inset-x-0
+bottom-0
+h-1/2
+bg-slate-100
+dark:bg-slate-800
+`;
+
+const ImageWrapper = tw.div`
+absolute
+inset-0
+`;
+
+const Image = tw.img`
+h-full
+w-full
+object-cover
+`;
+
+const ImageDiv = tw.div`
+absolute
+inset-0
+bg-slate-800
+opacity-60
+mix-blend-multiply
+`;
+
+const NoImageDiv = tw.div`
+absolute
+inset-0
+bg-slate-800
+mix-blend-multiply
+`;
+
+const HeaderWrapper = tw.div`
+z-50
+relative
+px-4
+py-4
+-mb-16
+`;
+
+const LeaderWrapper = tw.div`
+relative
+px-4
+py-16
+sm:px-6
+sm:py-24
+lg:py-32
+lg:px-8
+text-center
+`;
+
+const LeaderH1 = tw.h1`
+text-4xl
+md:max-w-2xl
+mx-auto
+font-bold
+font-serif
+pb-2
+lg:pb-4
+tracking-tight
+`;
+
+const LeaderSpan = tw.span`
+block
+font-sans
+text-white
+text-lg
+pb-0
+`;
+
+const TitleSpan = tw.span`
+block
+text-white
+font-serif
+mt-4
+`;
+
+const FollowerParagraph = tw.p`
+px-2
+italic
+font-light
+mt-2
+max-w-lg
+mx-auto
+lg:text-center
+text-base
+text-gray-300
+sm:max-w-3xl
+`;
+
+const ButtonWrapper = tw.div`
+mt-10
+max-w-sm
+mx-auto
+sm:max-w-none
+sm:flex
+sm:justify-center
+`;
